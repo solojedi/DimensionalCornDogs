db38210198910ee486da361e7f88b3c55c8b1560
solojedi <jerry@LAPTOP-27T4T79U>
2020-08-06 09:36:48 -0500

    -Added Logo image for Mod

--- src/main/java/net/dimensionalcorndog/procedures/NetherCornDogFoodEatenProcedure.java
+++ src/main/java/net/dimensionalcorndog/procedures/NetherCornDogFoodEatenProcedure.java
@@ -5,14 +5,12 @@ import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraft.world.dimension.DimensionType;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.potion.Effects;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.network.play.server.SPlayerAbilitiesPacket;
 import net.minecraft.network.play.server.SPlaySoundEventPacket;
 import net.minecraft.network.play.server.SPlayEntityEffectPacket;
 import net.minecraft.network.play.server.SChangeGameStatePacket;
 import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.Entity;
 
 import net.dimensionalcorndog.DimensionalCornDogModVariables;
@@ -73,8 +71,6 @@ public class NetherCornDogFoodEatenProcedure extends DimensionalCornDogModElemen
 					capability.syncPlayerVariables(entity);
 				});
 			}
-			if (entity instanceof LivingEntity)
-				((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.NAUSEA, (int) 60, (int) 1));
 		}
 	}
 }
